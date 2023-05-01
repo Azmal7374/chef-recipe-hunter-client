de1f17321ff0519447f3cffeaba651791afd3d37
Azmal <azmalgazi8@gmail.com>
2023-05-01 23:37:25 +0600

    client setpu complete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+ import React from 'react';
+ 
+ const App = () => {
+  return (
+    <div>
+      <h2 className="text-center font-bold text-3xl mt-5">Hello </h2>
+    </div>
+  );
+ };
+ 
+ export default App;
